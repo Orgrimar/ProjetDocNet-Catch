ab9b1a5f8b4ab192e2617c29f0f5f59e88d2a552
Jérôme <polatoj@gmail.com>
2017-04-06 14:08:12 +0200

    Ajoutez des fichiers projet.

---
+++ ProjetDocNet-Catch/Catcheur.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetDocNet_Catch
+{
+    abstract class Catcheur
+    {
+        public string Nom { get; set; }
+        public int Hp { get; set; }
+        public int Str { get; set; }
+        public int Def { get; set; }
+
+
+        enum Status { Disponible, Morgue, Hospital };
+        enum Action { Frapper, Defendre, CompetenceSpeciale };
+
+        int status = (int)Status.Disponible;
+        
+        public Catcheur(string nom, int hp, int str, int def)
+        {
+            Nom = nom;
+            Hp = hp;
+            Str = str;
+            Def = def;
+        }
+
+        public void ChoixAction()
+        {
+            Random rnd = new Random();
+            int action = rnd.Next(1, 3);
+            switch (action)
+            {
+                case 1:
+                    Frapper(target, attaquant);
+                    break;
+                case 2:
+                    Defendre(target, attaquant);
+                    break;
+                case 3:
+                    CompSpec(target, attaquant);
+                    break;
+            }
+        }
+
+        public void Frapper(Catcheur target, Catcheur attaquant)
+        {
+            target.Hp -= attaquant.Str;
+        }
+
+        public void Defendre(Catcheur target, Catcheur attaquant)
+        {
+            attaquant.Str -= target.Def;
+        }
+
+        public void CompSpec(Catcheur target, Catcheur attaquant)
+        {
+            Random rnd = new Random();
+            int activation = rnd.Next();
+            if ((activation % 2) == 0)
+            {
+
+            }
+
+        }
+
+        List<Catcheur> combat = new List<Catcheur>();
+
+        public abstract void SoignerCatcheur(Catcheur target);
+    }
+}
---
+++ ProjetDocNet-Catch/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetDocNet_Catch
+{
+    class Program
+    {
+        static void Jeu()
+        {
+            int semaine = 0;
+            while (semaine < 8)
+            {
+                semaine++;
+                Console.WriteLine("Semaine " + semaine);
+                //Menu de la semaine
+
+                //Debreif
+
+                //Continuer ou quitter
+                if (semaine == 8)
+                {
+                    Console.WriteLine("Souhaitez-vous continuer ?");
+                    Console.WriteLine("1 - Oui");
+                    Console.WriteLine("2 - Non");
+                    int.TryParse(Console.ReadLine(), out int choix);
+                    if (choix == 1)
+                        semaine = 0;
+                    Console.WriteLine("Nouvelle saison");
+                }
+            }
+        }
+
+        static void MenuPrincipal()
+        {
+            Console.WriteLine("Bienvenue");
+            Console.WriteLine("1 - Jouer");
+            Console.WriteLine("2 - Quitter");
+            int.TryParse(Console.ReadLine(), out int choix);
+            if (choix == 1)
+                Jeu();
+        }
+
+        static void Appli()
+        {
+            bool quitter = false;
+            while (!quitter)
+            {
+                MenuPrincipal();
+
+                //Fin de programme
+                Console.WriteLine("Souhaitez-vous quitter ?");
+                Console.WriteLine("1 - Oui");
+                Console.WriteLine("2 - Non");
+                int.TryParse(Console.ReadLine(), out int choix);
+                if (choix == 1)
+                    quitter = true;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Appli();
+        }
+    }
+}
